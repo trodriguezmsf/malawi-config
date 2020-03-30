4b0f202c5a1a52090efe40928415053859fee61b
surajkumardas <surajkumar.das@thoughtworks.com>
2020-03-30 12:24:06 +0530

    MOBN-1126 | Suraj | Removing the delete query from emrapi.sqlSearch.otToBeScheduledQueue

--- openmrs/migrations/OT/patientListingQueueToBeScheduled.sql
+++ openmrs/migrations/OT/patientListingQueueToBeScheduled.sql
@@ -1,8 +1,4 @@
-DELETE
-FROM global_property
-WHERE property = 'emrapi.sqlSearch.otToBeScheduledQueue';
-SELECT uuid()
-INTO @uuid;
+SELECT uuid() INTO @uuid;
 
 INSERT INTO global_property (`property`, `property_value`, `description`, `uuid`)
 VALUES ('emrapi.sqlSearch.otToBeScheduledQueue',
