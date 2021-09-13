9c2bf14a5f9b4d095edaf20869ed8d48f448e3d9
tarunkumar-tw <tarunkumar.s@thoughtworks.com>
2021-09-13 17:10:49 +0530

    Tarun | MOBN-1695 | Add concepts for colposcopy form

---
+++ openmrs/migrations/Colposcopy/Colposcopy.sql
@@ -0,0 +1,57 @@
+set @concept_id = 0;
+set @concept_short_id = 0;
+set @concept_full_id = 0;
+set @count = 0;
+set @uuid = NULL;
+
+#Add Parent Concepts
+#Add Date Concepts
+call add_concept(@concept_id,@concept_short_id,@concept_full_id,"CS, Date/time recorded","Date recorded","Date","Misc",false);
+call add_concept(@concept_id,@concept_short_id,@concept_full_id,"CS, Date of appointment for results","Date of appointment for results","Date","Misc",false);
+
+#Add Text Concepts
+call add_concept(@concept_id,@concept_short_id,@concept_full_id,"CS, Reason for visit","Reason for visit","Text","Misc",false);
+call add_concept(@concept_id,@concept_short_id,@concept_full_id,"CS, Other/Description Vulva and Vagina examination, comment","Other/Description Vulva & Vagina examination, comment","Text","Misc",false);
+call add_concept(@concept_id,@concept_short_id,@concept_full_id,"CS, Aceto-white areas, Localisation","Aceto-white areas, Localisation","Text","Misc",false);
+call add_concept(@concept_id,@concept_short_id,@concept_full_id,"CS, Aceto-white areas, Description","Aceto-white areas, Description","Text","Misc",false);
+call add_concept(@concept_id,@concept_short_id,@concept_full_id,"CS, Other lesion, Localisation","Other lesion, Localisation","Text","Misc",false);
+call add_concept(@concept_id,@concept_short_id,@concept_full_id,"CS, Other lesion, Description","Other lesion, Description","Text","Misc",false);
+call add_concept(@concept_id,@concept_short_id,@concept_full_id,"CS, Biopsy localisation detail","Biopsy localisation detail","Text","Misc",false);
+
+#Add Coded Concepts
+call add_concept(@concept_id,@concept_short_id,@concept_full_id,"CS, Vulva and Vagina examination","Vulva & Vagina examination","Coded","Misc",false);
+call add_concept(@concept_id,@concept_short_id,@concept_full_id,"CS, Cervix visibility","Cervix visibility","Coded","Misc",false);
+call add_concept(@concept_id,@concept_short_id,@concept_full_id,"CS, Colposcopic findings","Colposcopic findings","Coded","Misc",false);
+call add_concept(@concept_id,@concept_short_id,@concept_full_id,"CS, Colposcopy conclusion","Colposcopy conclusion","Coded","Misc",false);
+call add_concept(@concept_id,@concept_short_id,@concept_full_id,"CS, Procedure performed","Procedure performed","Coded","Misc",false);
+call add_concept(@concept_id,@concept_short_id,@concept_full_id,"CS, Biopsy localisation","Biopsy localisation","Coded","Misc",false);
+
+#Add Numeric Concepts
+call add_concept(@concept_id,@concept_short_id,@concept_full_id,"CS, Aceto-white areas, Size","Aceto-white areas, Size","Numeric","Misc",false);
+call add_concept(@concept_id,@concept_short_id,@concept_full_id,"CS, Other lesion, Size","Other lesion, Size","Numeric","Misc",false);
+call add_concept(@concept_id,@concept_short_id,@concept_full_id,"CS, Biopsies, Number of specimen(s) collected","Biopsies, Number of specimen(s) collected","Numeric","Misc",false);
+
+#Add Numeric concepts to concept Numeric Table
+INSERT INTO concept_numeric (concept_id,hi_absolute,hi_critical,hi_normal,low_absolute,low_critical,low_normal,units,precise,display_precision)
+VALUES ((select concept_id from concept_name where name = "CS, Aceto-white areas, Size" and concept_name_type = "FULLY_SPECIFIED"  and locale = "en"  and voided = 0),NULL,NULL,NULL,NULL,NULL,NULL,"",1,1);
+INSERT INTO concept_numeric (concept_id,hi_absolute,hi_critical,hi_normal,low_absolute,low_critical,low_normal,units,precise,display_precision)
+VALUES ((select concept_id from concept_name where name = "CS, Other lesion, size" and concept_name_type = "FULLY_SPECIFIED"  and locale = "en"  and voided = 0),NULL,NULL,NULL,NULL,NULL,NULL,"",1,1);
+INSERT INTO concept_numeric (concept_id,hi_absolute,hi_critical,hi_normal,low_absolute,low_critical,low_normal,units,precise,display_precision)
+VALUES ((select concept_id from concept_name where name = "CS, Biopsies, Number of specimen(s) collected" and concept_name_type = "FULLY_SPECIFIED"  and locale = "en"  and voided = 0),NULL,NULL,NULL,NULL,NULL,NULL,"",1,1);
+
+#Add Child Concepts
+call add_concept(@concept_id,@concept_short_id,@concept_full_id,"Other lesion","Other lesion","N/A","Misc",false);
+call add_concept(@concept_id,@concept_short_id,@concept_full_id,"HSIL","HSIL","N/A","Misc",false);
+call add_concept(@concept_id,@concept_short_id,@concept_full_id,"Vulva condyloma acuminata","Vulva condyloma acuminata","N/A","Misc",false);
+call add_concept(@concept_id,@concept_short_id,@concept_full_id,"Biopsy of cervix","Biopsy of cervix","N/A","Misc",false);
+call add_concept(@concept_id,@concept_short_id,@concept_full_id,"Curettage of endocervix","Curettage of endocervix","N/A","Misc",false);
+call add_concept(@concept_id,@concept_short_id,@concept_full_id,"Vaginal condyloma acuminata","Vaginal condyloma acuminata","N/A","Misc",false);
+call add_concept(@concept_id,@concept_short_id,@concept_full_id,"Biopsy of endocervix","Biopsy of endocervix","N/A","Misc",false);
+call add_concept(@concept_id,@concept_short_id,@concept_full_id,"Cancer","Cancer","N/A","Misc",false);
+call add_concept(@concept_id,@concept_short_id,@concept_full_id,"Not fully visualised","Not fully visualised","N/A","Misc",false);
+call add_concept(@concept_id,@concept_short_id,@concept_full_id,"Benign","Benign","N/A","Misc",false);
+call add_concept(@concept_id,@concept_short_id,@concept_full_id,"LSIL","LSIL","N/A","Misc",false);
+call add_concept(@concept_id,@concept_short_id,@concept_full_id,"Aceto-white areas","Aceto-white areas","N/A","Misc",false);
+call add_concept(@concept_id,@concept_short_id,@concept_full_id,"Fully visualised","Fully visualised","N/A","Misc",false);
+call add_concept(@concept_id,@concept_short_id,@concept_full_id,"At lesion","At lesion","N/A","Misc",false);
+call add_concept(@concept_id,@concept_short_id,@concept_full_id,"At SCJ","At SCJ","N/A","Misc",false);
