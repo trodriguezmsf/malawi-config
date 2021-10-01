f911ae2e530743988b9f0166e9df2572a4324730
tarunkumar-tw <tarunkumar.s@thoughtworks.com>
2021-10-01 16:48:13 +0530

    Tarun | MOBN-1776 | Update colposcopy form concepts as per uat feedback

--- openmrs/migrations/Colposcopy/Colposcopy.sql
+++ openmrs/migrations/Colposcopy/Colposcopy.sql
@@ -7,7 +7,6 @@ set @uuid = NULL;
 #Add Parent Concepts
 #Add Date Concepts
 call add_concept(@concept_id,@concept_short_id,@concept_full_id,"CS, Date/time recorded","Date recorded","Date","Misc",false);
-call add_concept(@concept_id,@concept_short_id,@concept_full_id,"CS, Date of appointment for results","Date of appointment for results","Date","Misc",false);
 
 #Add Text Concepts
 call add_concept(@concept_id,@concept_short_id,@concept_full_id,"CS, Reason for visit","Reason for visit","Text","Misc",false);
@@ -17,6 +16,8 @@ call add_concept(@concept_id,@concept_short_id,@concept_full_id,"CS, Aceto-white
 call add_concept(@concept_id,@concept_short_id,@concept_full_id,"CS, Other lesion, Localisation","Other lesion, Localisation","Text","Misc",false);
 call add_concept(@concept_id,@concept_short_id,@concept_full_id,"CS, Other lesion, Description","Other lesion, Description","Text","Misc",false);
 call add_concept(@concept_id,@concept_short_id,@concept_full_id,"CS, Biopsy localisation detail","Biopsy localisation detail","Text","Misc",false);
+call add_concept(@concept_id,@concept_short_id,@concept_full_id,"CS, Aceto-white areas, Size","Aceto-white areas, Size","Text","Misc",false);
+call add_concept(@concept_id,@concept_short_id,@concept_full_id,"CS, Other lesion, Size","Other lesion, Size","Text","Misc",false);
 
 #Add Coded Concepts
 call add_concept(@concept_id,@concept_short_id,@concept_full_id,"CS, Vulva and Vagina examination","Vulva & Vagina examination","Coded","Misc",false);
@@ -27,17 +28,14 @@ call add_concept(@concept_id,@concept_short_id,@concept_full_id,"CS, Procedure p
 call add_concept(@concept_id,@concept_short_id,@concept_full_id,"CS, Biopsy localisation","Biopsy localisation","Coded","Misc",false);
 
 #Add Numeric Concepts
-call add_concept(@concept_id,@concept_short_id,@concept_full_id,"CS, Aceto-white areas, Size","Aceto-white areas, Size","Numeric","Misc",false);
-call add_concept(@concept_id,@concept_short_id,@concept_full_id,"CS, Other lesion, Size","Other lesion, Size","Numeric","Misc",false);
 call add_concept(@concept_id,@concept_short_id,@concept_full_id,"CS, Biopsies, Number of specimen(s) collected","Biopsies, Number of specimen(s) collected","Numeric","Misc",false);
+call add_concept(@concept_id,@concept_short_id,@concept_full_id,"CS, Date of appointment for results in","Date of appointment for results in","Numeric","Misc",false);
 
 #Add Numeric concepts to concept Numeric Table
 INSERT INTO concept_numeric (concept_id,hi_absolute,hi_critical,hi_normal,low_absolute,low_critical,low_normal,units,precise,display_precision)
-VALUES ((select concept_id from concept_name where name = "CS, Aceto-white areas, Size" and concept_name_type = "FULLY_SPECIFIED"  and locale = "en"  and voided = 0),NULL,NULL,NULL,NULL,NULL,NULL,"",1,1);
-INSERT INTO concept_numeric (concept_id,hi_absolute,hi_critical,hi_normal,low_absolute,low_critical,low_normal,units,precise,display_precision)
-VALUES ((select concept_id from concept_name where name = "CS, Other lesion, size" and concept_name_type = "FULLY_SPECIFIED"  and locale = "en"  and voided = 0),NULL,NULL,NULL,NULL,NULL,NULL,"",1,1);
-INSERT INTO concept_numeric (concept_id,hi_absolute,hi_critical,hi_normal,low_absolute,low_critical,low_normal,units,precise,display_precision)
 VALUES ((select concept_id from concept_name where name = "CS, Biopsies, Number of specimen(s) collected" and concept_name_type = "FULLY_SPECIFIED"  and locale = "en"  and voided = 0),NULL,NULL,NULL,NULL,NULL,NULL,"",1,1);
+INSERT INTO concept_numeric (concept_id,hi_absolute,hi_critical,hi_normal,low_absolute,low_critical,low_normal,units,precise,display_precision)
+VALUES ((select concept_id from concept_name where name = "CS, Date of appointment for results in" and concept_name_type = "FULLY_SPECIFIED"  and locale = "en"  and voided = 0),NULL,NULL,NULL,NULL,NULL,NULL,"weeks",1,1);
 
 #Add Child Concepts
 call add_concept(@concept_id,@concept_short_id,@concept_full_id,"Other lesion","Other lesion","N/A","Misc",false);
