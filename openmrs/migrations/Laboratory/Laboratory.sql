81ff89eaabc095582b99cf29d661f836d8f03d0a
tarunkumar-tw <tarunkumar.s@thoughtworks.com>
2021-10-04 11:40:34 +0530

    Tarun | MOBN-1776 | Update laboratory form concepts as per uat feedback

--- openmrs/migrations/Laboratory/Laboratory.sql
+++ openmrs/migrations/Laboratory/Laboratory.sql
@@ -6,7 +6,7 @@ set @uuid = NULL;
 
 #Add Parent Concepts
 #Add Date Concepts
-call add_concept(@concept_id,@concept_short_id,@concept_full_id,"LB, Date/time recorded","Date/time recorded","Date","Misc",false);
+call add_concept(@concept_id,@concept_short_id,@concept_full_id,"LB, Date recorded","Date recorded","Date","Misc",false);
 
 #Add Text Concepts
 call add_concept(@concept_id,@concept_short_id,@concept_full_id,"LB, Other type of visit","Other type of visit","Text","Misc",false);
@@ -44,27 +44,27 @@ call add_concept(@concept_id,@concept_short_id,@concept_full_id,"LB, HIV viral l
 
 #Add Numeric concepts to concept Numeric Table
 INSERT INTO concept_numeric (concept_id,hi_absolute,hi_critical,hi_normal,low_absolute,low_critical,low_normal,units,precise,display_precision)
-VALUES ((select concept_id from concept_name where name = "LB, Haemoglobin" and concept_name_type = "FULLY_SPECIFIED"  and locale = "en"  and voided = 0),NULL,NULL,NULL,NULL,NULL,NULL,"g/dl",1,1);
+VALUES ((select concept_id from concept_name where name = "LB, Haemoglobin" and concept_name_type = "FULLY_SPECIFIED"  and locale = "en"  and voided = 0),NULL,NULL,15,NULL,NULL,11.1,"g/dl",1,1);
 INSERT INTO concept_numeric (concept_id,hi_absolute,hi_critical,hi_normal,low_absolute,low_critical,low_normal,units,precise,display_precision)
-VALUES ((select concept_id from concept_name where name = "LB, Mean cell haemoglobin concentration" and concept_name_type = "FULLY_SPECIFIED"  and locale = "en"  and voided = 0),NULL,NULL,NULL,NULL,NULL,NULL,"g/dl",1,1);
+VALUES ((select concept_id from concept_name where name = "LB, Mean cell haemoglobin concentration" and concept_name_type = "FULLY_SPECIFIED"  and locale = "en"  and voided = 0),NULL,NULL,36,NULL,NULL,33,"g/dl",1,1);
 INSERT INTO concept_numeric (concept_id,hi_absolute,hi_critical,hi_normal,low_absolute,low_critical,low_normal,units,precise,display_precision)
-VALUES ((select concept_id from concept_name where name = "LB, Serum creatinine" and concept_name_type = "FULLY_SPECIFIED"  and locale = "en"  and voided = 0),NULL,NULL,NULL,NULL,NULL,NULL,"mg/dl",1,1);
+VALUES ((select concept_id from concept_name where name = "LB, Serum creatinine" and concept_name_type = "FULLY_SPECIFIED"  and locale = "en"  and voided = 0),NULL,NULL,1.4,NULL,NULL,0.1,"mg/dl",1,1);
 INSERT INTO concept_numeric (concept_id,hi_absolute,hi_critical,hi_normal,low_absolute,low_critical,low_normal,units,precise,display_precision)
-VALUES ((select concept_id from concept_name where name = "LB, Fasting blood glucose measurement" and concept_name_type = "FULLY_SPECIFIED"  and locale = "en"  and voided = 0),NULL,NULL,NULL,NULL,NULL,NULL,"mg/dl",1,1);
+VALUES ((select concept_id from concept_name where name = "LB, Fasting blood glucose measurement" and concept_name_type = "FULLY_SPECIFIED"  and locale = "en"  and voided = 0),NULL,NULL,110,NULL,NULL,70,"mg/dl",1,1);
 INSERT INTO concept_numeric (concept_id,hi_absolute,hi_critical,hi_normal,low_absolute,low_critical,low_normal,units,precise,display_precision)
-VALUES ((select concept_id from concept_name where name = "LB, CD4 count" and concept_name_type = "FULLY_SPECIFIED"  and locale = "en"  and voided = 0),NULL,NULL,NULL,NULL,NULL,NULL,"cells/µL",1,1);
+VALUES ((select concept_id from concept_name where name = "LB, CD4 count" and concept_name_type = "FULLY_SPECIFIED"  and locale = "en"  and voided = 0),NULL,NULL,1500,NULL,NULL,500,"cells/µL",1,1);
 INSERT INTO concept_numeric (concept_id,hi_absolute,hi_critical,hi_normal,low_absolute,low_critical,low_normal,units,precise,display_precision)
-VALUES ((select concept_id from concept_name where name = "LB, Serum glutamic-oxaloacetic transaminase" and concept_name_type = "FULLY_SPECIFIED"  and locale = "en"  and voided = 0),NULL,NULL,NULL,NULL,NULL,NULL,"U/L",1,1);
+VALUES ((select concept_id from concept_name where name = "LB, Serum glutamic-oxaloacetic transaminase" and concept_name_type = "FULLY_SPECIFIED"  and locale = "en"  and voided = 0),NULL,NULL,34,NULL,NULL,6,"U/L",1,1);
 INSERT INTO concept_numeric (concept_id,hi_absolute,hi_critical,hi_normal,low_absolute,low_critical,low_normal,units,precise,display_precision)
-VALUES ((select concept_id from concept_name where name = "LB, Serum glutamic-pyruvic transaminase" and concept_name_type = "FULLY_SPECIFIED"  and locale = "en"  and voided = 0),NULL,NULL,NULL,NULL,NULL,NULL,"U/L",1,1);
+VALUES ((select concept_id from concept_name where name = "LB, Serum glutamic-pyruvic transaminase" and concept_name_type = "FULLY_SPECIFIED"  and locale = "en"  and voided = 0),NULL,NULL,40,NULL,NULL,10,"U/L",1,1);
 INSERT INTO concept_numeric (concept_id,hi_absolute,hi_critical,hi_normal,low_absolute,low_critical,low_normal,units,precise,display_precision)
-VALUES ((select concept_id from concept_name where name = "LB, Alkaline phosphatase" and concept_name_type = "FULLY_SPECIFIED"  and locale = "en"  and voided = 0),NULL,NULL,NULL,NULL,NULL,NULL,"IU/L",1,1);
+VALUES ((select concept_id from concept_name where name = "LB, Alkaline phosphatase" and concept_name_type = "FULLY_SPECIFIED"  and locale = "en"  and voided = 0),NULL,NULL,126,NULL,NULL,30,"IU/L",1,1);
 INSERT INTO concept_numeric (concept_id,hi_absolute,hi_critical,hi_normal,low_absolute,low_critical,low_normal,units,precise,display_precision)
-VALUES ((select concept_id from concept_name where name = "LB, Creatinine clearance test" and concept_name_type = "FULLY_SPECIFIED"  and locale = "en"  and voided = 0),NULL,NULL,NULL,NULL,NULL,NULL,"mm/24",1,1);
+VALUES ((select concept_id from concept_name where name = "LB, Creatinine clearance test" and concept_name_type = "FULLY_SPECIFIED"  and locale = "en"  and voided = 0),NULL,NULL,18,NULL,NULL,9,"mm/24",1,1);
 INSERT INTO concept_numeric (concept_id,hi_absolute,hi_critical,hi_normal,low_absolute,low_critical,low_normal,units,precise,display_precision)
-VALUES ((select concept_id from concept_name where name = "LB, Serum sodium" and concept_name_type = "FULLY_SPECIFIED"  and locale = "en"  and voided = 0),NULL,NULL,NULL,NULL,NULL,NULL,"mmol/L",1,1);
+VALUES ((select concept_id from concept_name where name = "LB, Serum sodium" and concept_name_type = "FULLY_SPECIFIED"  and locale = "en"  and voided = 0),NULL,NULL,145,NULL,NULL,135,"mmol/L",1,1);
 INSERT INTO concept_numeric (concept_id,hi_absolute,hi_critical,hi_normal,low_absolute,low_critical,low_normal,units,precise,display_precision)
-VALUES ((select concept_id from concept_name where name = "LB, Serum albumin" and concept_name_type = "FULLY_SPECIFIED"  and locale = "en"  and voided = 0),NULL,NULL,NULL,NULL,NULL,NULL,"mg/dL",1,1);
+VALUES ((select concept_id from concept_name where name = "LB, Serum albumin" and concept_name_type = "FULLY_SPECIFIED"  and locale = "en"  and voided = 0),NULL,NULL,5.5,NULL,NULL,3.5,"mg/dL",1,1);
 INSERT INTO concept_numeric (concept_id,hi_absolute,hi_critical,hi_normal,low_absolute,low_critical,low_normal,units,precise,display_precision)
 VALUES ((select concept_id from concept_name where name = "LB, HIV viral load" and concept_name_type = "FULLY_SPECIFIED"  and locale = "en"  and voided = 0),NULL,NULL,NULL,NULL,NULL,NULL,"",1,1);
 
@@ -84,3 +84,37 @@ call add_concept(@concept_id,@concept_short_id,@concept_full_id,"Regular follow 
 call add_concept(@concept_id,@concept_short_id,@concept_full_id,"AB+","AB+","N/A","Misc",false);
 call add_concept(@concept_id,@concept_short_id,@concept_full_id,"3+","3+","N/A","Misc",false);
 call add_concept(@concept_id,@concept_short_id,@concept_full_id,"2+","2+","N/A","Misc",false);
+call add_concept(@concept_id,@concept_short_id,@concept_full_id,"Growth","Growth","N/A","Misc",false);
+call add_concept(@concept_id,@concept_short_id,@concept_full_id,"No Growth","No Growth","N/A","Misc",false);
+
+#Add Help Text to Concepts
+INSERT INTO concept_description (concept_id,description,locale,creator,date_created,changed_by,date_changed,uuid)
+VALUES ((select concept_id from concept_name where name = "LB, Haemoglobin" and concept_name_type = "FULLY_SPECIFIED" and locale = "en" and voided = 0),
+"Normal range: 11.1 - 15","en",1,now(),NULL,NULL,uuid());
+INSERT INTO concept_description (concept_id,description,locale,creator,date_created,changed_by,date_changed,uuid)
+VALUES ((select concept_id from concept_name where name = "LB, Mean cell haemoglobin concentration" and concept_name_type = "FULLY_SPECIFIED" and locale = "en" and voided = 0),
+"Normal range: 33 - 36","en",1,now(),NULL,NULL,uuid());
+INSERT INTO concept_description (concept_id,description,locale,creator,date_created,changed_by,date_changed,uuid)
+VALUES ((select concept_id from concept_name where name = "LB, Serum creatinine" and concept_name_type = "FULLY_SPECIFIED" and locale = "en" and voided = 0),
+"Normal range: 0.1 - 1.4","en",1,now(),NULL,NULL,uuid());
+INSERT INTO concept_description (concept_id,description,locale,creator,date_created,changed_by,date_changed,uuid)
+VALUES ((select concept_id from concept_name where name = "LB, Fasting blood glucose measurement" and concept_name_type = "FULLY_SPECIFIED" and locale = "en" and voided = 0),
+"Normal range: 70 - 110","en",1,now(),NULL,NULL,uuid());
+INSERT INTO concept_description (concept_id,description,locale,creator,date_created,changed_by,date_changed,uuid)
+VALUES ((select concept_id from concept_name where name = "LB, Serum glutamic-oxaloacetic transaminase" and concept_name_type = "FULLY_SPECIFIED" and locale = "en" and voided = 0),
+"Normal range: 6 - 34","en",1,now(),NULL,NULL,uuid());
+INSERT INTO concept_description (concept_id,description,locale,creator,date_created,changed_by,date_changed,uuid)
+VALUES ((select concept_id from concept_name where name = "LB, Serum glutamic-pyruvic transaminase" and concept_name_type = "FULLY_SPECIFIED" and locale = "en" and voided = 0),
+"Normal range: 10 - 40","en",1,now(),NULL,NULL,uuid());
+INSERT INTO concept_description (concept_id,description,locale,creator,date_created,changed_by,date_changed,uuid)
+VALUES ((select concept_id from concept_name where name = "LB, Alkaline phosphatase" and concept_name_type = "FULLY_SPECIFIED" and locale = "en" and voided = 0),
+"Normal range: 30 - 126","en",1,now(),NULL,NULL,uuid());
+INSERT INTO concept_description (concept_id,description,locale,creator,date_created,changed_by,date_changed,uuid)
+VALUES ((select concept_id from concept_name where name = "LB, Creatinine clearance test" and concept_name_type = "FULLY_SPECIFIED" and locale = "en" and voided = 0),
+"Normal range: 9 - 18","en",1,now(),NULL,NULL,uuid());
+INSERT INTO concept_description (concept_id,description,locale,creator,date_created,changed_by,date_changed,uuid)
+VALUES ((select concept_id from concept_name where name = "LB, Serum sodium" and concept_name_type = "FULLY_SPECIFIED" and locale = "en" and voided = 0),
+"Normal range: 135 - 145","en",1,now(),NULL,NULL,uuid());
+INSERT INTO concept_description (concept_id,description,locale,creator,date_created,changed_by,date_changed,uuid)
+VALUES ((select concept_id from concept_name where name = "LB, Serum albumin" and concept_name_type = "FULLY_SPECIFIED" and locale = "en" and voided = 0),
+"Normal range: 3.5 -5.5","en",1,now(),NULL,NULL,uuid());
