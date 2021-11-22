114aa3cf3dc2ecf68d79c62c7a6fa5e0203d2c94
supriya <supriya.muppiri@thoughtworks.com>
2021-11-22 15:47:12 +0530

    Supriya | MOBN-1793 | Update concept names in Symptoms Assessment form

--- openmrs/migrations/SymptomsAssessment/SymptomsAssessment.sql
+++ openmrs/migrations/SymptomsAssessment/SymptomsAssessment.sql
@@ -16,18 +16,18 @@ call add_concept(@concept_id,@concept_short_id,@concept_full_id,"SA, Visit type"
 call add_concept(@concept_id,@concept_short_id,@concept_full_id,"SA, Signs and symptoms","Signs and symptoms","Coded","Misc",false);
 call add_concept(@concept_id,@concept_short_id,@concept_full_id,"SA, Fatigue severity of symptom","Fatigue severity","Coded","Misc",false);
 call add_concept(@concept_id,@concept_short_id,@concept_full_id,"SA, Sleep disorder severity of symptom","Sleep disorder severity","Coded","Misc",false);
-call add_concept(@concept_id,@concept_short_id,@concept_full_id,"SA, Nausea and vomiting  severity of symptom","Nause and vomiting severity","Coded","Misc",false);
+call add_concept(@concept_id,@concept_short_id,@concept_full_id,"SA, Nausea and vomiting severity of symptom","Nausea and vomiting severity","Coded","Misc",false);
 call add_concept(@concept_id,@concept_short_id,@concept_full_id,"SA, Poor appetite severity of symptom","Poor appetite severity","Coded","Misc",false);
 call add_concept(@concept_id,@concept_short_id,@concept_full_id,"SA, Shortness of breath severity of symptom","Shortness of breath severity","Coded","Misc",false);
 call add_concept(@concept_id,@concept_short_id,@concept_full_id,"SA, Constipation severity of symptom","Constipation severity","Coded","Misc",false);
 call add_concept(@concept_id,@concept_short_id,@concept_full_id,"SA, Urge incontinence of urine severity of symptom","Urine incontinence severity","Coded","Misc",false);
 call add_concept(@concept_id,@concept_short_id,@concept_full_id,"SA, Dysuria severity of symptom","Dysuria severity","Coded","Misc",false);
 call add_concept(@concept_id,@concept_short_id,@concept_full_id,"SA, Oral lesion severity of symptom","Oral lesion severity","Coded","Misc",false);
-call add_concept(@concept_id,@concept_short_id,@concept_full_id,"SA, Abnormal vaginal bleeing severity of symptom","Abnormal vaginal bleeding  severity","Coded","Misc",false);
+call add_concept(@concept_id,@concept_short_id,@concept_full_id,"SA, Abnormal vaginal bleeding severity of symptom","Abnormal vaginal bleeding severity","Coded","Misc",false);
 call add_concept(@concept_id,@concept_short_id,@concept_full_id,"SA, Postcoital and contact bleeding severity of symptom","Contact bleeding severity","Coded","Misc",false);
 call add_concept(@concept_id,@concept_short_id,@concept_full_id,"SA, Vaginal odor severity of symptom","Vaginal odor severity","Coded","Misc",false);
 call add_concept(@concept_id,@concept_short_id,@concept_full_id,"SA, Vaginal discharge severity of symptom","Vaginal discharge severity","Coded","Misc",false);
-call add_concept(@concept_id,@concept_short_id,@concept_full_id,"SA, Dyspareunia  severity of symptom","Dyspareunia severity","Coded","Misc",false);
+call add_concept(@concept_id,@concept_short_id,@concept_full_id,"SA, Dyspareunia severity of symptom","Dyspareunia severity","Coded","Misc",false);
 call add_concept(@concept_id,@concept_short_id,@concept_full_id,"SA, Other symptoms severity","Other symptoms severity","Coded","Misc",false);
 
 #Add Child Concepts
