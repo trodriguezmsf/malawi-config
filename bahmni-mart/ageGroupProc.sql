0d1ab46b87b51af23657a6ac1b8bb7852ff3db25
venkybavisetti <bavisetti.venkatesh@thoughtworks.com>
2022-01-05 19:25:12 +0530

    MOBN-1853| venky| fix mart observations

--- bahmni-mart/ageGroupProc.sql
+++ bahmni-mart/ageGroupProc.sql
@@ -6,8 +6,8 @@ AS $$
 BEGIN
   IF EXTRACT(YEAR FROM (age(process_start_date, birthdate))) BETWEEN 0 AND 17
   THEN RETURN '0-17';
-  ELSEIF EXTRACT(YEAR FROM (age(process_start_date, birthdate))) BETWEEN 18 AND 20
-    THEN RETURN '18-20';
+  ELSEIF EXTRACT(YEAR FROM (age(process_start_date, birthdate))) BETWEEN 18 AND 29
+    THEN RETURN '18-29';
   ELSEIF (EXTRACT(YEAR FROM (age(process_start_date, birthdate))) BETWEEN 30 AND 49)
     THEN RETURN '30-49';
   ELSEIF EXTRACT(YEAR FROM (age(process_start_date, birthdate))) BETWEEN 50 AND 65
