210a4a21d43f340fb362a7734bd553a3f67c26b3
binduak <tbindu@thoughtworks.com>
2022-01-06 18:40:57 +0530

    Bindu | MOBN-1865 | Add field length validations for non-auto populated address fields

--- openmrs/apps/registration/fieldValidation.js
+++ openmrs/apps/registration/fieldValidation.js
@@ -22,5 +22,35 @@ Bahmni.Registration.customValidator = {
             return value && value.length <= 50;
         },
         errorMessage: "REGISTRATION_HOUSE_NUMBER_STREET_TEXT_ERROR_KEY"
+    },
+    "address.countyDistrict": {
+        method: function (name, value, personAttributeDetails) {
+            return value && value.length <= 250;
+        },
+        errorMessage: "REGISTRATION_VILLAGE_HEADMAN_TEXT_ERROR_KEY"
+    },
+    "address.address5": {
+        method: function (name, value, personAttributeDetails) {
+            return value && value.length <= 250;
+        },
+        errorMessage: "REGISTRATION_NEAREST_HEALTH_FACILITY_TO_HOME_TEXT_ERROR_KEY"
+    },
+    "address.cityVillage": {
+        method: function (name, value, personAttributeDetails) {
+            return value && value.length <= 250;
+        },
+        errorMessage: "REGISTRATION_VILLAGE_NEIGHBORHOOD_TEXT_ERROR_KEY"
+    },
+    "address.address6": {
+        method: function (name, value, personAttributeDetails) {
+            return value && value.length <= 250;
+        },
+        errorMessage: "REGISTRATION_ADDRESS_DESCRIPTION_TEXT_ERROR_KEY"
+    },
+    "address.address4": {
+        method: function (name, value, personAttributeDetails) {
+            return value && value.length <= 250;
+        },
+        errorMessage: "REGISTRATION_TRADITIONAL_AUTHORITY_OR_TOWN_TEXT_ERROR_KEY"
     }
 };
