d5dd7b4ea03e80f7ad54aaecdd19a76f07b66f60
KiruthigaBaskaran <kiruthiga.baskaran@thoughtworks.com>
2022-10-31 16:22:31 +0530

    Kiruthiga |MOBN-2041| Add feedback changes for Bed management

---
+++ openmrs/migrations/BedManagement/InpatientDepartment_Bed_Layout_1.sql
@@ -0,0 +1,18 @@
+CALL BED_ALLOCATION('17', 'Inpatient Department Ward',1,1);
+CALL BED_ALLOCATION('16', 'Inpatient Department Ward',1,2);
+CALL BED_ALLOCATION('15', 'Inpatient Department Ward',1,3);
+CALL BED_ALLOCATION('14', 'Inpatient Department Ward',1,4);
+CALL BED_ALLOCATION('9', 'Inpatient Department Ward',2,5);
+CALL BED_ALLOCATION('10', 'Inpatient Department Ward',2,4);
+CALL BED_ALLOCATION('11', 'Inpatient Department Ward',2,3);
+CALL BED_ALLOCATION('12', 'Inpatient Department Ward',2,2);
+CALL BED_ALLOCATION('13', 'Inpatient Department Ward',2,1);
+
+CALL BED_ALLOCATION('1', 'Inpatient Department Ward',1,8);
+CALL BED_ALLOCATION('2', 'Inpatient Department Ward',1,9);
+CALL BED_ALLOCATION('3', 'Inpatient Department Ward',1,10);
+CALL BED_ALLOCATION('4', 'Inpatient Department Ward',1,11);
+CALL BED_ALLOCATION('5', 'Inpatient Department Ward',1,12);
+CALL BED_ALLOCATION('6', 'Inpatient Department Ward',2,12);
+CALL BED_ALLOCATION('7', 'Inpatient Department Ward',2,11);
+CALL BED_ALLOCATION('8', 'Inpatient Department Ward',2,10);
