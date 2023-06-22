68b6ec672efd922f55ab686dcd285cf1ec7110ea
KiruthigaBaskaran <99712264+KiruthigaBaskaran@users.noreply.github.com>
2023-06-22 18:35:30 +0530

    Kiruthiga |MOBN-2116| Change VAS Score IPD Assessment form (#50)

--- openmrs/migrations/IPDAssessment/IPDAssessment.sql
+++ openmrs/migrations/IPDAssessment/IPDAssessment.sql
@@ -14,7 +14,7 @@ call add_concept(@concept_id,@concept_short_id,@concept_full_id,"IPDA, VAS score
 
 #Add Numeric concepts to concept Numeric Table
 INSERT INTO concept_numeric (concept_id,hi_absolute,hi_critical,hi_normal,low_absolute,low_critical,low_normal,units,precise,display_precision)
-VALUES ((select concept_id from concept_name where name = "IPDA, VAS score" and concept_name_type = "FULLY_SPECIFIED"  and locale = "en"  and voided = 0),99.9,NULL,NULL,0,NULL,NULL,"",1,1);
+VALUES ((select concept_id from concept_name where name = "IPDA, VAS score" and concept_name_type = "FULLY_SPECIFIED"  and locale = "en"  and voided = 0),10,NULL,NULL,-0.0000000001,NULL,NULL,"",0,1);
 
 #Add Text Concepts
 call add_concept(@concept_id,@concept_short_id,@concept_full_id,"IPDA, Main Complain","Main Complain","Text","Misc",false);
