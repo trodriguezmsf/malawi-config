c9eedf73c77cad9a94819b5a2f122d24e064e88e
venkybavisetti <bavisetti.venkatesh@thoughtworks.com>
2023-08-11 11:45:05 +0530

    Venky| MOBN-2184 | add conmplications querry to fetch complications data

--- openmrs/migrations/CustomSql/complicationsDisplayControl.sql
+++ openmrs/migrations/CustomSql/complicationsDisplayControl.sql
@@ -6,8 +6,7 @@ INSERT INTO global_property (property, property_value, description, uuid)
 "SELECT
   patient_encounters.encounter_id,
   patient_encounters.patient_id,
-  surgicalHysterectomyComplication.value AS Hysterectomy,
-  surgicalOvarianComplication.value AS Ovarian
+  complications.value AS `Patient Complications`
 FROM
   (
     SELECT
@@ -18,7 +17,7 @@ FROM
       INNER JOIN encounter e ON p.person_id = e.patient_id
       AND e.voided IS FALSE
       AND p.voided IS FALSE
-      AND p.uuid = 'd5128555-b273-4c3c-a48b-79118225c2f4'
+      AND p.uuid = '5ff6d27d-1b7b-487a-9dd6-0d70b05548c6'
       INNER JOIN visit v ON e.visit_id = v.visit_id
       AND v.voided IS FALSE
       AND v.visit_type_id = 5
@@ -38,28 +37,13 @@ FROM
       AND cn.voided IS FALSE
       AND o.voided IS FALSE
       AND cn.concept_name_type = 'FULLY_SPECIFIED'
-      AND cn.name IN ('HY, Intraoperative complication', 'OV, Intraoperative complication')
+      AND cn.name IN (
+        'HY, Intraoperative complication',
+        'OV, Intraoperative complication',
+        'VU, Intraoperative complication'
+      )
       LEFT OUTER JOIN concept_name coded_fscn ON coded_fscn.concept_id = o.value_coded
       AND coded_fscn.concept_name_type = 'FULLY_SPECIFIED'
       AND coded_fscn.voided IS FALSE
-  ) surgicalHysterectomyComplication ON surgicalHysterectomyComplication.encounter_id = patient_encounters.encounter_id
-  LEFT JOIN (
-    SELECT
-      o.encounter_id,
-      o.person_id,
-      GROUP_CONCAT(coded_fscn.name SEPARATOR ', ') AS 'value'
-    FROM
-      obs o
-      INNER JOIN concept_name cn ON cn.concept_id = o.concept_id
-      AND cn.voided IS FALSE
-      AND o.voided IS FALSE
-      AND cn.concept_name_type = 'FULLY_SPECIFIED'
-      AND cn.name = 'OV, Intraoperative complication'
-      LEFT OUTER JOIN concept_name coded_fscn ON coded_fscn.concept_id = o.value_coded
-      AND coded_fscn.concept_name_type = 'FULLY_SPECIFIED'
-      AND coded_fscn.voided IS FALSE
-    GROUP BY
-      o.encounter_id,
-      o.person_id
-  ) surgicalOvarianComplication ON surgicalOvarianComplication.encounter_id = patient_encounters.encounter_id"
+  ) complications ON complications.encounter_id = patient_encounters.encounter_id"
 , 'Complications', @uuid);
